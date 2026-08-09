e5d6a58aa435c301263edeb32bc518f850b0fcef
Shivamkr-cod <namkeenshivam156@gmail.com>
2026-08-09 17:28:53 +0530

    Untrack test scripts and add them to .gitignore

--- test-parse.js
+++
@@ -1,3 +0,0 @@
-const { parse } = require('pg-connection-string');
-const connStr = "postgresql://shivam:npg_LJcjo8M3Zagi@ep-proud-pine-axeucb91.c-4.us-east-2.aws.neon.tech/MediMeet?sslmode=require&channel_binding=require";
-console.log(parse(connStr));
